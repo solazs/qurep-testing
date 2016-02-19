9df7182ff243a9a2ef1e61b4282e5806d7fbdcbb
solazs <soltesz.balazs.91@gmail.com>
2016-02-19 02:26:55 +0100

    fixed tests, stuff should work as intended

--- tests/QuRePTestBundle/Controller/DefaultControllerTest.php
+++ tests/QuRePTestBundle/Controller/DefaultControllerTest.php
@@ -244,6 +244,56 @@ class DefaultControllerTest extends RestTestCase
     /**
      * @depends testBulkUpdate
      */
+    public function testExpand()
+    {
+        $client = static::createClient();
+        $client->request(
+            "POST",
+            "/users/" . self::$users[2]['id'],
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE'          => 'application/json',
+                'HTTP_X-Requested-With' => 'XMLHttpRequest'
+            ),
+            json_encode(array("parent" => self::$users[0]['id']))
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertJsonResponse($response, 201);
+
+        $responseData = json_decode($response->getContent(), true);
+
+        if ($responseData === null){
+            $this->fail("Not valid JSON or null response!");
+        }
+
+        $this->assertEntityEquals(self::$users[2], $responseData);
+
+        $child = self::$users[2];
+        $child['parent'] = self::$users[0];
+        self::$users[2] = $child;
+
+        $client->request('GET', '/users/' . self::$users[2]['id'] . '?expand=parent');
+
+        $response = $client->getResponse();
+
+        $this->assertJsonResponse($response);
+
+        $responseData = json_decode($response->getContent(), true);
+
+        if ($responseData === null){
+            $this->fail("Not valid JSON or null response!");
+        }
+
+        $this->assertEntityEquals(self::$users[2], $responseData);
+        
+    }
+
+    /**
+     * @depends testExpand
+     */
     function testBulkDelete(){
         $client = static::createClient();
         $client->request(
@@ -255,13 +305,23 @@ class DefaultControllerTest extends RestTestCase
                 'CONTENT_TYPE'          => 'application/json',
                 'HTTP_X-Requested-With' => 'XMLHttpRequest'
             ),
-            json_encode(self::$users)
+            json_encode([self::$users[2], self::$users[1]])
         );
 
 
         $response = $client->getResponse();
         $this->assertEquals(204, $response->getStatusCode());
 
-        $this->testEmpty();
+        $client->request('GET', '/users');
+
+        $response = $client->getResponse();
+
+        $responseData = json_decode($response->getContent(), true);
+
+        if ($responseData === null){
+            $this->fail("Not valid JSON or null response!");
+        }
+
+        $this->assertEntityEquals([self::$users[0]], $responseData);
     }
 }
